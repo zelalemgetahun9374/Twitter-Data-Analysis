98a5985afcfe20169a7aa6aa2521435c9e28c254
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-06-21 22:35:23 +0300

    fix: fixed function convert_to_numbers

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -44,10 +44,11 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-
-        ----
-        ----
+        df["polarity"] = pd.to_numeric(df["polarity"])
+        df["subjectivity"] = pd.to_numeric(df["subjectivity"])
+        df["retweet_count"] = pd.to_numeric(df["retweet_count"])
+        df["favorite_count"] = pd.to_numeric(df["favorite_count"])
+        df["friends_count "] = pd.to_numeric(df["polarity"])
 
         return df
 
