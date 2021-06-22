a19e88f22a0efb60e4df9788c6a0d02c5ab45f79
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-06-22 22:00:41 +0300

    fix: fixed function find_retweet_count

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -93,8 +93,13 @@ class TweetDfExtractor:
         return favourite_count
 
     def find_retweet_count(self) -> list:
-        retweet_count = [x['retweeted_status']['retweet_count']
+        try:
+            retweet_count = [x['retweeted_status']['retweet_count']
                          for x in self.tweets_list]
+
+        except KeyError:
+            retweet_count = [None for x in self.tweets_list]
+        
         return retweet_count
 
     def find_hashtags(self) -> list:
