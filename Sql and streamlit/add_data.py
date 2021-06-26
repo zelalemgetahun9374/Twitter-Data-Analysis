b99037ff7d512841cb0ed21c828baa96c98d8a5d
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-06-27 01:20:50 +0300

    fix: fix schema and insert statement

--- Sql and streamlit/add_data.py
+++ Sql and streamlit/add_data.py
@@ -86,9 +86,11 @@ def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
     -------
 
     """
-    cols_2_drop = ['Unnamed: 0', 'timestamp', 'sentiment', 'possibly_sensitive', 'original_text']
+    # cols_2_drop = ['possibly_sensitive']
     try:
-        df = df.drop(columns=cols_2_drop, axis=1)
+        # df = df.drop(columns=cols_2_drop, axis=1)
+        values={"hashtags": "", "user_mentions": ""}
+        df = df.fillna(value=values)
         df = df.fillna(0)
     except KeyError as e:
         print("Error:", e)
@@ -117,12 +119,11 @@ def insert_to_tweet_table(dbName: str, df: pd.DataFrame, table_name: str) -> Non
     df = preprocess_df(df)
 
     for _, row in df.iterrows():
-        sqlQuery = f"""INSERT INTO {table_name} (created_at, source, clean_text, polarity, subjectivity, language,
-                    favorite_count, retweet_count, original_author, screen_count, followers_count, friends_count,
-                    hashtags, user_mentions, place)
-             VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+        sqlQuery = f"""INSERT INTO {table_name} (created_at, source, original_text, clean_text, sentiment, polarity, subjectivity, language,
+                    favorite_count, retweet_count, original_author, followers_count, friends_count, hashtags, user_mentions, place)
+             VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
         data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9], row[10], row[11],
-                row[12], row[13], row[14])
+                row[12], row[13], row[14], row[15])
 
         try:
             # Execute the SQL command
@@ -188,5 +189,6 @@ if __name__ == "__main__":
     createTables(dbName='tweets')
 
     df = pd.read_csv('../data/processed_tweets.csv')
+    print(df.head())
 
-    insert_to_tweet_table(dbName='tweets', df=df, table_name='TweetInformation')
+    insert_to_tweet_table(dbName='tweets', df=df.head(), table_name='TweetInformation')
